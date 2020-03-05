a9496ee1897bf92454e21644c2be8b017c8eb62e
Stefani8511 <rougirel.stephanie@gmail.com>
2020-03-05 22:57:02 +0100

    test mot de passe avec caracteres

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+const regExp1 = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regExp2 = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/;
+
 
 form.addEventListener("submit", event => {
   event.preventDefault();
@@ -11,6 +14,8 @@ form.addEventListener("submit", event => {
   checkLength(password, 3, 15);
   checkEmail(email);
   passwordMatch(password, password2);
+  checkPassword(password, password2);
+ 
 });
 
 const checkRequired = inputArr => {
@@ -26,7 +31,7 @@ const checkLength = (input, min, max) => {
   if (input.value.length < min) {
     showError(input, `Ce champ doit comporter au minimum ${min} caractères !`);
   } else if (input.value.length > max) {
-    showError(input, `Ce champ ne doit pas excéder ${max} caractères !`);
+    showError(input, `Ce champ doit contenir au moins ${max}!`);
   } else {
     showSuccess(input);
   }
@@ -42,32 +47,41 @@ const showSuccess = input => {
   formControl.className = " form-control success";
 };
 const checkEmail = input => {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regExp1 = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (re.test(input.value)) {
+  if (regExp1.test(input.value)) {
     showSuccess(input);
   } else {
     showError(input, "L'email n'est pas valide !");
   }
 };
-const passwordMatch = (input1, input2) => {
-  if (input1.value !== input2.value) {
+const passwordMatch = (input1, input2) =>{
+  const regExp2 = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/;
+  if (input1.value !== input2.value ){
 
 // SI la valeur de l'input 1 est différente de la valeur de l'input 2 alors:
 
     showError(input2, " Le mot de passe ne correspond pas");
-  } else {
+  
+  }else if(input1.value !== (regExp2.test(input.value))){
+    showError(input, "Mot de passe non conforme")
+  }
+   else {
     
 // SINON :
     showSuccess(input);
   }
 };
 
-const passwordMatch = (input1, input2) => {
-if (input1.value('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W)#', $password)) {
-     showSuccess (input,"Mot de passe conforme")
 
-}else {
+//Possibililité fonctionnelle que sur un mot de passe1 :-(
+  /*
+const checkPassword = (input ) => {
+  const re2 = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/;
+if (re2.test (input.value))  {
+      showSuccess (input,"Mot de passe conforme")
+}
+else {
     showError(input, "Mot de passe non conforme")
 }  
-}
+}*/
